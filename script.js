68e9091a352c5755fdda6ff3ffb7d48397e4a255
BrunoPineda <89885824+BrunoPineda@users.noreply.github.com>
2022-08-22 00:36:05 -0500

    nose

---
+++ script.js
@@ -0,0 +1,66 @@
+const $ = selector => document.querySelector(selector)
+const $table = $('#table')
+const $file = document.getElementById('file1');
+
+const $SenFiles = document.getElementById('formSendFiles');
+
+
+const $submit = $('#submit')
+const $results = $('#results')
+
+const $buttonCreateFile = document.getElementById('createFile');
+const $buttonUpdateFile = document.getElementById('updateFile');
+const $buttonDeleteFile = document.getElementById('deleteFile');
+let files;
+$SenFiles.addEventListener('click', () => {
+    event.preventDefault()
+    files = this.files
+    console.log(files)
+});
+
+$buttonCreateFile.addEventListener('click', () => {
+    /*hola*/
+    Swal.fire({
+        title: '¿Que nombre quieres ponerle a tu carpeta?',
+        input: 'text',
+        inputAttributes: {
+            autocapitalize: 'off'
+        },
+        showCancelButton: true,
+        confirmButtonText: 'Crear',
+        showLoaderOnConfirm: true,
+        preConfirm: (name) => {
+            var location = document.URL;
+            var example = "https://api-rest-brucorp1.herokuapp.com/upload/folder/a!proMaster!crack"
+
+            var url = "https://api-rest-brucorp1.herokuapp.com/upload/folder/" + name;
+
+            var xhr = new XMLHttpRequest();
+            xhr.open("POST", url);
+
+            xhr.setRequestHeader("Content-Type", "application/json");
+            xhr.setRequestHeader("Content-Length", "0");
+
+            xhr.onreadystatechange = function() {
+                if (xhr.readyState === 4) {
+                    console.log(xhr.status);
+                    console.log(xhr.responseText);
+                }
+            };
+
+            xhr.send();
+
+
+        },
+        allowOutsideClick: () => !Swal.isLoading()
+    }).then((result) => {
+        if (result.isConfirmed) {
+            Swal.fire({
+                title: `Tu carpeta se creo correctamente `
+            })
+            setTimeout((function() {
+                window.location.reload();
+            }), 500);
+        }
+    })
+});
